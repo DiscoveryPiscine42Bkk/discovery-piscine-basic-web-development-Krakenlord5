1349384d05c605612c8d4a0b16509eae5c6e3a83
Chananyu Chinnawuth <chchinna@z2t8c1.42bangkok.com>
2025-04-28 20:15:21 +0700

    abc

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -1,4 +1,10 @@
 function clickBalloon()
 {
-    
+    document.getElementById("balloon").style.width += 10;
+    document.getElementById("balloon").style.height += 10;
+}
+
+function hoverBalloon()
+{
+
 }
