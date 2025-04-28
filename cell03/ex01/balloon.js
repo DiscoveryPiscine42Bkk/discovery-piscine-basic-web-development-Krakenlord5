bdc67d04a9e981e10623c50bd358c9fa9d6aa4a6
Krakenlord <chananyu1301@gmail.com>
2025-04-28 23:40:59 +0700

    cell03 ex01

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -1,10 +1,45 @@
 function clickBalloon()
 {
-    document.getElementById("balloon").style.width += 10;
-    document.getElementById("balloon").style.height += 10;
+    const balloon = document.getElementById("balloon");
+    // change size
+    var newWidth = balloon.offsetWidth + 10;
+    var newHeight = balloon.offsetHeight + 10;
+    if (newWidth > 420)
+    {
+        newWidth = 200;
+        newHeight = 200;
+    }
+    balloon.style.width = newWidth.toString() + "px";
+    balloon.style.height = newHeight.toString() + "px";
+
+    // change col
+    const currentCol = window.getComputedStyle(balloon).backgroundColor;
+    var newCol;
+
+    if (currentCol === "rgb(255, 0, 0)") { newCol = "rgb(0, 255, 0)"}
+    else if (currentCol === "rgb(0, 255, 0)") { newCol = "rgb(0, 0, 255)"}
+    else { newCol = "rgb(255, 0, 0)"}
+
+    balloon.style.backgroundColor = newCol;
 }
 
-function hoverBalloon()
+function leaveBalloon()
 {
+    const balloon = document.getElementById("balloon");
+    if (balloon.offsetWidth > 200)
+    {
+        var newWidth = balloon.offsetWidth - 5;
+        var newHeight = balloon.offsetHeight - 5;
+        balloon.style.width = newWidth.toString() + "px";
+        balloon.style.height = newHeight.toString() + "px";
+    }
+    // change col
+    const currentCol = window.getComputedStyle(balloon).backgroundColor;
+    var newCol;
 
+    if (currentCol === "rgb(255, 0, 0)") { newCol = "rgb(0, 0, 255)"}
+    else if (currentCol === "rgb(0, 0, 255)") { newCol = "rgb(0, 255, 0)"}
+    else { newCol = "rgb(255, 0, 0)"}
+
+    balloon.style.backgroundColor = newCol;
 }
